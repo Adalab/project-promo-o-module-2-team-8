099ff19fcb71939e29ca4f0d0955c5f650a31bc8
vicblanc92 <vicbr92@gmail.com>
2021-12-11 19:50:34 +0100

    add collapsed javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 'use strict';
+const collapsableTitle = document.querySelector('.js-collapsable_title');
 
-console.log('>> Ready :)');
+function handlerClickHeader() {
+  const collapsableContent = document.querySelector('.js-collapsable_content');
+  collapsableContent.classList.toggle('collapsed');
+}
+
+collapsableTitle.addEventListener('click', handlerClickHeader);
